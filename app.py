2d57f319a471d5ae8566d8ad3b425cb2f77d56e4
Maury Quijada <maury.quijada@gmail.com>
2014-05-05 21:45:08 -0400

    Continued work on user profile.

--- app.py
+++ app.py
@@ -16,33 +16,40 @@ def get_user_profile(userid):
         raise Exception
 
     # Connect to the database.
-    db = MySQLdb.connect(host='localhost', port=3306, user='5cbookfinder', passwd='g4G5IkDOM3a91EV', db='5cbookfinder', cursorclass=MySQLdb.cursors.DictCursor)
+    db = MySQLdb.connect(host='localhost', port=3306, user='5cbookfinder', 
+        passwd='g4G5IkDOM3a91EV', db='5cbookfinder', 
+        cursorclass=MySQLdb.cursors.DictCursor)
     cursor = db.cursor()
 
     # Select the right user and raise an error if we don't have an exact match.
-    cursor.execute('SELECT user_id FROM Users WHERE user_id = %s', userid)
+    # TODO: Cache these results!
+    cursor.execute('SELECT * FROM Users WHERE user_id = %s', userid)
     rows_affected = cursor.rowcount
+    user = cursor.fetchone()
     if int(rows_affected) is not 1:
         raise Exception
 
-    # Otherwise, get the books they track and sell.
-    #cursor.execute('SELECT B.* FROM Books B INNER JOIN UserTracksBook UTB ON UTB.book_isbn = B.book_isbn WHERE UTB.user_id = %s', userid)
-    #tracked_books = cursor.fetchall()
+    # Get the books that we've recently listed.
+    # TODO.
 
-    cursor.execute('SELECT BFS.listing_id, BFS.book_isbn, BFS.created_at, BFS.edition, BFS.price, BFS.book_condition, B.*  FROM BooksForSale BFS, Books B, UserSellsBook USB WHERE B.book_isbn = BFS.book_isbn AND USB.listing_id = BFS.listing_id AND USB.user_id = %s', userid)
-    selling_books = cursor.fetchall()
+    # Get the books that are currently being sold that are on their wishlist.
+    cursor.execute("""SELECT B.*, BFS.created_at, BFS.price, USB.user_id AS 'owner', BFSS.description AS 'condition' FROM Books B, UserTracksBook UTB, BooksForSale BFS, UserSellsBook USB, BooksForSaleStatus BFSS WHERE UTB.book_isbn = B.book_isbn AND BFS.book_isbn = B.book_isbn AND BFS.status = 1 AND BFSS.id = BFS.book_condition AND UTB.user_id = %s""", userid)
+    wishlist_selling = cursor.fetchall()
 
-    # Books we're tracking
-    #for b in tracked_books:
-
-    # Books we're selling.
+    # Then, get the books that they themselves are selling.
+    cursor.execute("""SELECT BFS.listing_id, BFS.book_isbn, 
+        BFS.created_at, BFS.edition, BFS.price, BFS.book_condition, B.* 
+        FROM BooksForSale BFS, Books B, UserSellsBook USB WHERE 
+        B.book_isbn = BFS.book_isbn AND USB.listing_id = BFS.listing_id AND 
+        USB.user_id = %s""", userid)
+    user_selling = cursor.fetchall()
 
     # Get the book image from Google Books API.
 
 
     db.close()
 
-    return render_template('user_profile.html')
+    return render_template('user_profile.html', wishlist_selling=wishlist_selling, user_selling=user_selling, user=user)
 
 @app.route('/book/<isbn>')
 def get_book_information(isbn):
